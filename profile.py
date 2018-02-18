75d26fb9320921e5225707399db8209315e1a449
linhbngo <lngo@clemson.edu>
2018-02-17 23:18:19 -0500

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,25 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
+
+pc = portal.Context()
+request = rspec.Request()
+
+tourDescription = "This profile provides a baseline Ubuntu 14.4 image"
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
+request.addTour(tour)
+
+node = request.RawPC("node" + str(i))
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
+
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="touch test.txt"))   
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec(request)
