f38d926b51b38bf8f83a7b439a7e2b305c8a2c8e
linhbngo <lngo@clemson.edu>
2018-02-18 10:08:37 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,24 +5,41 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = rspec.Request()
 
-tourDescription = "This profile automatically launches a Hadoop cluster with 1 headnode and 3 datanode on 4 VMs"
+pc.defineParameter("workerCount",
+                   "Number of Hadoop DataNodes",
+                   portal.ParameterType.INTEGER, 3)
+
+pc.defineParameter("controllerHost", "Name of NameNode",
+                   portal.ParameterType.STRING, "node0", advanced=True,
+                   longDescription="The short name of the Hadoop NameNode.  You shold leave \
+                   this alone unless you really want the hostname to change.")
+
+params = pc.bindParameters()
+
+tourDescription = "This profile provides a configurable Hadoop testbed with one NameNode \
+and customizable number of DataNodes."
+
+tourInstructions = \
+  """
+### Basic Instructions
+Once your experiment nodes have booted, and this profile's configuration scripts \
+have finished deploying Hadoop inside your experiment, you'll be able to visit 
+[the HDFS Web UI](http://{host-%s}:9870) (approx. 5-15 minutes).  
+""" % (params.controllerHost)
 
 #
-# Setup the Tour info with the above description.
+# Setup the Tour info with the above description and instructions.
 #  
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
+tour.Instructions(IG.Tour.MARKDOWN,tourInstructions)
 request.addTour(tour)
 
-
-# Create a Request object to start building the RSpec.
-#request = portal.context.makeRequestRSpec()
-#request 
 # Create a link with type LAN
 link = request.LAN("lan")
 
 # Generate the nodes
-for i in range(4):
+for i in range(params.workerCount + 1):
     node = request.XenVM("node" + str(i))
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
     iface = node.addInterface("if" + str(i))
@@ -39,6 +56,8 @@ for i in range(4):
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo cp /local/repository/slaves /opt/hadoop-3.0.0/etc/hadoop/workers"))
     node.addService(rspec.Execute(shell="/bin/sh",
+                                  command="sudo cp /local/repository/core-site.xml /opt/hadoop-3.0.0/etc/hadoop/core-site.xml"))
+    node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo apt-get update -y"))
     node.addService(rspec.Execute(shell="/bin/sh",
                                   command="sudo apt-get install -y default-jdk"))
@@ -46,7 +65,7 @@ for i in range(4):
                                   command="export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/"))    
     if i != 0:
         node.addService(rspec.Execute(shell="/bin/sh",
-                                      command="sudo sleep 60"))
+                                      command="sudo sleep 30"))
         node.addService(rspec.Execute(shell="/bin/sh",
                                       command="sudo /opt/hadoop-3.0.0/bin/hadoop-daemon.sh start datanode"))
     else:
