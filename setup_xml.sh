864212b3344d60e573021798200bc4b9efb68758
Linh B Ngo <lngo@clemson.edu>
2018-10-01 22:07:44 -0400

    Update setup_xml.sh

--- setup_xml.sh
+++ setup_xml.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+sudo cp /local/repository/hadoop-env.sh /opt/hadoop-3.1.1/etc/hadoop/hadoop-env.sh
+
 sudo sed -i -e "s/HOSTNAME/$(ssh -o StrictHostKeyChecking=no root@namenode hostname)/g" /local/repository/core-site.xml
 sudo cp /local/repository/core-site.xml /opt/hadoop-3.1.1/etc/hadoop/core-site.xml
 
