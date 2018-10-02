beec7c4484d07ddf3334ca4eda3aa14ec604100a
Linh B Ngo <lngo@clemson.edu>
2018-10-02 10:24:25 -0400

    Create port_forwarding.sh

---
+++ port_forwarding.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+sudo sysctl net.ipv4.ip_forward=1
+echo $(ifconfig enp1s0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+sudo iptables -t nat -A PREROUTING -p tcp $(ifconfig enp1s0 | grep "inet " | awk -F'[: ]+' '{ print $4 }') --dport 8089 -j DNAT --to-destination 192.168.1.1:8088
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
