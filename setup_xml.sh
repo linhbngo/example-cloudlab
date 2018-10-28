35260421e1f67ff1f899f6adcf483836aa71a5c1
Linh B Ngo <lngo@clemson.edu>
2018-10-28 17:30:23 -0400

    Update setup_xml.sh

--- setup_xml.sh
+++ setup_xml.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 
-sudo /bin/cp -rf /local/repository/hadoop-env.sh /opt/hadoop-3.1.1/etc/hadoop/hadoop-env.sh
+sudo /bin/cp -rf /local/repository/xml/hadoop-env.sh /opt/hadoop-3.1.1/etc/hadoop/hadoop-env.sh
 
-sudo /bin/cp -rf /local/repository/core-site.xml /opt/hadoop-3.1.1/etc/hadoop/core-site.xml
+sudo /bin/cp -rf /local/repository/xml/core-site.xml /opt/hadoop-3.1.1/etc/hadoop/core-site.xml
 
-sudo /bin/cp -rf /local/repository/yarn-site.xml /opt/hadoop-3.1.1/etc/hadoop/yarn-site.xml
+sudo /bin/cp -rf /local/repository/xml/yarn-site.xml /opt/hadoop-3.1.1/etc/hadoop/yarn-site.xml
 
-sudo /bin/cp -rf /local/repository/mapred-site.xml /opt/hadoop-3.1.1/etc/hadoop/mapred-site.xml
+sudo /bin/cp -rf /local/repository/xml/mapred-site.xml /opt/hadoop-3.1.1/etc/hadoop/mapred-site.xml
