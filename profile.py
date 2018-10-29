bf688cfb67dfb7fee9f01013bca8c7903e74df66
Linh B Ngo <lngo@clemson.edu>
2018-10-28 22:54:04 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -76,6 +76,8 @@ for i in range(params.workerCount + 1):
                                       command="sudo /opt/hadoop-3.1.1/bin/yarn --daemon start resourcemanager"))
         node.addService(rspec.Execute(shell="/bin/sh",
                                       command="sudo bash /local/repository/port_forwarding.sh"))
+        node.addService(rspec.Execute(shell="/bin/sh",
+                                      command="sudo bash /local/repository/create_account.sh"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec(request)
