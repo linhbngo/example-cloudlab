487790b049a27e4dcfc9549531453742074b3160
Linh B Ngo <lngo@clemson.edu>
2018-10-31 23:11:05 -0400

    Update create_account.sh

--- create_account.sh
+++ create_account.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -x
-sudo su
-bash -c "echo 'HADOOP_HOME=/opt/hadoop-3.1.1/' >> /etc/profile"
-bash -c "echo 'export PATH=/opt/hadoop-3.1.1/bin:$PATH' >> /etc/profile"
 
-source /etc/profile
-cd /users
-i=0
-while read line
-do
-  array[ $i ]="$line"
-  (( i++ ))
-done < <(find . -mindepth 1 -maxdepth 1 -type d \( ! -iname ".*" \) | sed 's|^\./||g')
+if hostname | grep -q namenode; then
+  cd /users
+  i=0
+  while read line
+  do
+    array[ $i ]="$line"
+    (( i++ ))
+  done < <(find . -mindepth 1 -maxdepth 1 -type d \( ! -iname ".*" \) | sed 's|^\./||g')
 
-groupadd hadoop
-
-for j in "${array[@]}"
-do
-  usermod -a -G hadoop $j
-  hdfs dfs -mkdir -p /user/$j
-  hdfs dfs -chown $j:hadoop /user/$j
-  sudo hdfs dfs -chmod 755 /user/$j
-done
-
+  groupadd hadoop
+  for j in "${array[@]}"
+  do
+    usermod -a -G hadoop $j
+    /usr/local/hadoop-2.7.3/hdfs dfs -mkdir -p /user/$j
+    /usr/local/hadoop-2.7.3/hdfs dfs -chown $j:hadoop /user/$j
+    /usr/local/hadoop-2.7.3/hdfs dfs -chmod 755 /user/$j
+  done
+fi
