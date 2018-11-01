eca407440f410ca07c8d0962064cb1ae0077c4f2
Linh B Ngo <lngo@clemson.edu>
2018-10-31 23:24:42 -0400

    Update create_account.sh

--- create_account.sh
+++ create_account.sh
@@ -14,8 +14,8 @@ if hostname | grep -q namenode; then
   for j in "${array[@]}"
   do
     usermod -a -G hadoop $j
-    /usr/local/hadoop-2.7.3/hdfs dfs -mkdir -p /user/$j
-    /usr/local/hadoop-2.7.3/hdfs dfs -chown $j:hadoop /user/$j
-    /usr/local/hadoop-2.7.3/hdfs dfs -chmod 755 /user/$j
+    /usr/local/hadoop-2.7.3/bin/hdfs dfs -mkdir -p /user/$j
+    /usr/local/hadoop-2.7.3/bin/hdfs dfs -chown $j:hadoop /user/$j
+    /usr/local/hadoop-2.7.3/bin/hdfs dfs -chmod 755 /user/$j
   done
 fi
